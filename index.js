f6f172af347bf174afdecf749ed3ee62d2a3c504
jakubfiala <mu201jf@gold.ac.uk>
2017-03-07 10:11:35 +0000

    added checking for whether the template filename ends with .tpl.php

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const exec = require('child_process').exec;
  * @param      {object}   context   The context, which will become $variables in the template
  * @return     {Promise}  a Promise which resolves to the rendered HTML
  */
-const render = function(filePath, context) {
+const render = function(filePath, context = {}) {
+  if (!/\.tpl\.php$/.test(filePath)) {
+    return new Promise((res, rej) => rej(`File must have the .tpl.php extension. Filename is: ${filePath}`));
+  }
+
   const cmd = `php ${__dirname}/transformer.php ${filePath} '${JSON.stringify(context)}'`;
 
   return new Promise((resolve, reject) => {
---
+++ spec/tpl/not_tpl.php
@@ -0,0 +1,3 @@
+<p>
+This file is not a <code>.tpl.php</code> file. It should not be rendered.
+</p>
